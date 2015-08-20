df3408ef1f147d1a563c0824068886db323daae4
Alex Taradov <taradov@gmail.com>
2015-08-20 11:28:51 -0700

    Fixes for SAM4S and other multi-plane devices

--- target_cm4.c
+++ target_cm4.c
@@ -43,11 +43,12 @@
 #define AIRCR                  0xe000ed0c
 
 #define CHIPID_CIDR            0x400e0740
+#define CHIPID_EXID            0x400e0744
 
-#define EEFC_FMR               0x400e0a00
-#define EEFC_FCR               0x400e0a04
-#define EEFC_FSR               0x400e0a08
-#define EEFC_FRR               0x400e0a0c
+#define EEFC_FMR(n)            (0x400e0a00 + (n) * 0x200)
+#define EEFC_FCR(n)            (0x400e0a04 + (n) * 0x200)
+#define EEFC_FSR(n)            (0x400e0a08 + (n) * 0x200)
+#define EEFC_FRR(n)            (0x400e0a0c + (n) * 0x200)
 #define FSR_FRDY               1
 
 #define CMD_GETD               0x5a000000
@@ -60,8 +61,10 @@
 typedef struct
 {
   uint32_t  chip_id;
+  uint32_t  chip_exid;
   char      *name;
   uint32_t  flash_start;
+  uint32_t  n_planes;
   uint32_t  flash_size;
   uint32_t  page_size;
 } device_t;
@@ -70,12 +73,23 @@ typedef struct
 /*- Variables ---------------------------------------------------------------*/
 static device_t devices[] =
 {
-  { 0x247e0ae0, "SAM G53G19 Rev A",	0x00400000, 	512*1024,	512 },
-  { 0x247e0ae8, "SAM G53N19 Rev A",	0x00400000, 	512*1024,	512 },
-  { 0x247e0ae1, "SAM G53G19 Rev B",	0x00400000, 	512*1024,	512 },
-  { 0x247e0ae9, "SAM G53N19 Rev B",	0x00400000, 	512*1024,	512 },
-  { 0x24570ae0, "SAM G55J19 ES",	0x00400000, 	512*1024,	512 },
-  { 0, "", 0, 0, 0 },
+  { 0x247e0ae0, 0x00000000, "SAM G53G19 Rev A",	0x00400000, 1, 512*1024,	512 },
+  { 0x247e0ae8, 0x00000000, "SAM G53N19 Rev A",	0x00400000, 1, 512*1024,	512 },
+  { 0x247e0ae1, 0x00000000, "SAM G53G19 Rev B",	0x00400000, 1, 512*1024,	512 },
+  { 0x247e0ae9, 0x00000000, "SAM G53N19 Rev B",	0x00400000, 1, 512*1024,	512 },
+  { 0x24570ae0, 0x00000000, "SAM G55J19 ES",	0x00400000, 1, 512*1024,	512 },
+  { 0x29a70ee0, 0x00000000, "SAM 4SD32C",	0x00400000, 2, 1024*1024,	512 },
+  { 0X29970ee0, 0x00000000, "SAM 4SD32B",	0x00400000, 2, 1024*1024,	512 },
+  { 0X29a70ce0, 0x00000000, "SAM 4SD16C",	0x00400000, 2, 512*1024,	512 },
+  { 0X29970ce0, 0x00000000, "SAM 4SD16B",	0x00400000, 2, 512*1024,	512 },
+  { 0X28a70ce0, 0x00000000, "SAM 4SA16C",	0x00400000, 1, 1024*1024,	512 },
+  { 0X28970ce0, 0x00000000, "SAM 4SA16B",	0x00400000, 1, 1024*1024,	512 },
+  { 0x289c0ce0, 0x00000000, "SAM 4S16B",	0x00400000, 1, 1024*1024,	512 },
+  { 0x28ac0ce0, 0x00000000, "SAM 4S16C",	0x00400000, 1, 1024*1024,	512 },
+  { 0x289c0ae0, 0x00000000, "SAM 4S8B",		0x00400000, 1, 512*1024,	512 },
+  { 0x28ac0ae0, 0x00000000, "SAM 4S8C",		0x00400000, 1, 512*1024,	512 },
+
+  { 0, 0, "", 0, 0, 0, 0 },
 };
 
 static device_t *device;
@@ -85,7 +99,7 @@ static device_t *device;
 //-----------------------------------------------------------------------------
 static void target_cm4_select(void)
 {
-  uint32_t chip_id;
+  uint32_t chip_id, chip_exid;
 
   // Stop the core
   dap_write_word(DHCSR, 0xa05f0003);
@@ -93,34 +107,38 @@ static void target_cm4_select(void)
   dap_write_word(AIRCR, 0xfa050004);
 
   chip_id = dap_read_word(CHIPID_CIDR);
+  chip_exid = dap_read_word(CHIPID_EXID);
 
   for (device = devices; device->chip_id > 0; device++)
   {
-    if (device->chip_id == chip_id)
+    if (device->chip_id == chip_id && device->chip_exid == chip_exid)
     {
       uint32_t fl_id, fl_size, fl_page_size, fl_nb_palne, fl_nb_lock;
 
       verbose("Target: %s\n", device->name);
 
-      dap_write_word(EEFC_FCR, CMD_GETD);
-      while (0 == (dap_read_word(EEFC_FSR) & FSR_FRDY));
+      for (uint32_t plane = 0; plane < device->n_planes; plane++)
+      {
+        dap_write_word(EEFC_FCR(plane), CMD_GETD);
+        while (0 == (dap_read_word(EEFC_FSR(plane)) & FSR_FRDY));
 
-      fl_id = dap_read_word(EEFC_FRR);
-      check(fl_id, "Cannot read flash descriptor, check Erase pin state");
+        fl_id = dap_read_word(EEFC_FRR(plane));
+        check(fl_id, "Cannot read flash descriptor, check Erase pin state");
 
-      fl_size = dap_read_word(EEFC_FRR);
-      check(fl_size == device->flash_size, "Invalid reported Flash size (%d)", fl_size);
+        fl_size = dap_read_word(EEFC_FRR(plane));
+        check(fl_size == device->flash_size, "Invalid reported Flash size (%d)", fl_size);
 
-      fl_page_size = dap_read_word(EEFC_FRR);
-      check(fl_page_size == device->page_size, "Invalid reported page size (%d)", fl_page_size);
+        fl_page_size = dap_read_word(EEFC_FRR(plane));
+        check(fl_page_size == device->page_size, "Invalid reported page size (%d)", fl_page_size);
 
-      fl_nb_palne = dap_read_word(EEFC_FRR);
-      for (uint32_t i = 0; i < fl_nb_palne; i++)
-        dap_read_word(EEFC_FRR);
+        fl_nb_palne = dap_read_word(EEFC_FRR(plane));
+        for (uint32_t i = 0; i < fl_nb_palne; i++)
+          dap_read_word(EEFC_FRR(plane));
 
-      fl_nb_lock =  dap_read_word(EEFC_FRR);
-      for (uint32_t i = 0; i < fl_nb_lock; i++)
-        dap_read_word(EEFC_FRR);
+        fl_nb_lock =  dap_read_word(EEFC_FRR(plane));
+        for (uint32_t i = 0; i < fl_nb_lock; i++)
+          dap_read_word(EEFC_FRR(plane));
+      }
 
       return;
     }
@@ -130,12 +148,22 @@ static void target_cm4_select(void)
 }
 
 //-----------------------------------------------------------------------------
+static void target_cm4_deselect(void)
+{
+  dap_write_word(DEMCR, 0x00000000);
+  dap_write_word(DHCSR, 0xa05f0000);
+}
+
+//-----------------------------------------------------------------------------
 static void target_cm4_erase(void)
 {
   verbose("Erasing... ");
 
-  dap_write_word(EEFC_FCR, CMD_EA);
-  while (0 == (dap_read_word(EEFC_FSR) & FSR_FRDY));
+  for (uint32_t plane = 0; plane < device->n_planes; plane++)
+    dap_write_word(EEFC_FCR(plane), CMD_EA);
+
+  for (uint32_t plane = 0; plane < device->n_planes; plane++)
+    while (0 == (dap_read_word(EEFC_FSR(plane)) & FSR_FRDY));
 
   verbose("done.\n");
 }
@@ -145,7 +173,8 @@ static void target_cm4_lock(void)
 {
   verbose("Locking... ");
 
-  dap_write_word(EEFC_FCR, CMD_SGPB | (0 << 8));
+  // It is enough to lock just one plane to lock the entire device
+  dap_write_word(EEFC_FCR(0), CMD_SGPB | (0 << 8));
 
   verbose("done.\n");
 }
@@ -154,15 +183,16 @@ static void target_cm4_lock(void)
 static void target_cm4_program(char *name)
 {
   uint32_t addr = device->flash_start;
-  uint32_t size, number_of_pages;
+  uint32_t flash_size = device->flash_size * device->n_planes;
+  uint32_t size, number_of_pages, plane;
   uint32_t offs = 0;
   uint8_t *buf;
 
-  buf = buf_alloc(device->flash_size);
+  buf = buf_alloc(flash_size);
 
-  size = load_file(name, buf, device->flash_size);
+  size = load_file(name, buf, flash_size);
 
-  memset(&buf[size], 0xff, device->flash_size - size);
+  memset(&buf[size], 0xff, flash_size - size);
 
   verbose("Programming...");
 
@@ -170,8 +200,10 @@ static void target_cm4_program(char *name)
 
   for (uint32_t page = 0; page < number_of_pages; page += 8)
   {
-    dap_write_word(EEFC_FCR, CMD_EPA | ((page | 1) << 8));
-    while (0 == (dap_read_word(EEFC_FSR) & FSR_FRDY));
+    plane = page / (device->flash_size / device->page_size);
+
+    dap_write_word(EEFC_FCR(plane), CMD_EPA | ((page | 1) << 8));
+    while (0 == (dap_read_word(EEFC_FSR(plane)) & FSR_FRDY));
 
     verbose(".");
   }
@@ -187,8 +219,10 @@ static void target_cm4_program(char *name)
       offs += SECTOR_SIZE;
     }
 
-    dap_write_word(EEFC_FCR, CMD_WP | (page << 8));
-    while (0 == (dap_read_word(EEFC_FSR) & FSR_FRDY));
+    plane = page / (device->flash_size / device->page_size);
+
+    dap_write_word(EEFC_FCR(plane), CMD_WP | (page << 8));
+    while (0 == (dap_read_word(EEFC_FSR(plane)) & FSR_FRDY));
 
     verbose(".");
   }
@@ -202,14 +236,15 @@ static void target_cm4_program(char *name)
 static void target_cm4_verify(char *name)
 {
   uint32_t addr = device->flash_start;
+  uint32_t flash_size = device->flash_size * device->n_planes;
   uint32_t size, block_size;
   uint32_t offs = 0;
   uint8_t *bufa, *bufb;
 
-  bufa = buf_alloc(device->flash_size);
+  bufa = buf_alloc(flash_size);
   bufb = buf_alloc(SECTOR_SIZE);
 
-  size = load_file(name, bufa, device->flash_size);
+  size = load_file(name, bufa, flash_size);
 
   verbose("Verification...");
 
@@ -247,12 +282,13 @@ static void target_cm4_verify(char *name)
 //-----------------------------------------------------------------------------
 static void target_cm4_read(char *name)
 {
-  uint32_t size = device->flash_size;
+  uint32_t flash_size = device->flash_size * device->n_planes;
+  uint32_t size = flash_size;
   uint32_t addr = device->flash_start;
   uint32_t offs = 0;
   uint8_t *buf;
 
-  buf = buf_alloc(device->flash_size);
+  buf = buf_alloc(flash_size);
 
   verbose("Reading...");
 
@@ -269,7 +305,7 @@ static void target_cm4_read(char *name)
     verbose(".");
   }
 
-  save_file(name, buf, device->flash_size);
+  save_file(name, buf, flash_size);
 
   buf_free(buf);
 
@@ -279,11 +315,12 @@ static void target_cm4_read(char *name)
 //-----------------------------------------------------------------------------
 target_ops_t target_cm4_ops = 
 {
-  .select  = target_cm4_select,
-  .erase   = target_cm4_erase,
-  .lock    = target_cm4_lock,
-  .program = target_cm4_program,
-  .verify  = target_cm4_verify,
-  .read    = target_cm4_read,
+  .select   = target_cm4_select,
+  .deselect = target_cm4_deselect,
+  .erase    = target_cm4_erase,
+  .lock     = target_cm4_lock,
+  .program  = target_cm4_program,
+  .verify   = target_cm4_verify,
+  .read     = target_cm4_read,
 };
 
